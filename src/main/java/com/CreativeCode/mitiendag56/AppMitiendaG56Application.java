99800dfc5b3ffbf57087beabca3f4c980912262c
Jacke <ing.jacke@gmail.com>
2022-12-03 14:38:07 +0100

    configuration proyect

---
+++ src/main/java/com/CreativeCode/mitiendag56/AppMitiendaG56Application.java
@@ -0,0 +1,13 @@
+package com.CreativeCode.mitiendag56;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMitiendaG56Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppMitiendaG56Application.class, args);
+	}
+
+}
